e1768756bf588398438536b9eaad4e28f1f6ea48
Edward Crompton <edward.crompton@gmail.com>
2025-07-05 23:05:55 +0100

    Revert reset pin in example code to correct align with new diagram

--- examples/example_rxtx_threaded.py
+++ examples/example_rxtx_threaded.py
@@ -21,7 +21,7 @@ def receiveFunction(radio):
 # Bonnet https://www.adafruit.com/product/4072
 # You should adjust them to whatever matches your radio
 with Radio(FREQ_915MHZ, node_id, network_id, isHighPower=True, verbose=False,
-           interruptPin=18, resetPin=29, spiDevice=1) as radio:
+           interruptPin=15, resetPin=22, spiDevice=1) as radio:
     print ("Starting loop...")
 
     # Create a thread to run receiveFunction in the background and start it
